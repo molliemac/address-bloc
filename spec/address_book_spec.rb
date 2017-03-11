6385ba76cd17e110de0f34f0d635f1e828cb6bbf
Mollie McIntyre <mollie.mcintyre9@gmail.com>
2017-03-10 23:11:02 -0800

    Assignment 7 completed: created and ran tests for entries2.csv

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -103,6 +103,37 @@ require_relative '../models/address_book'
        check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
      end
    end
+
+     # Test that AddressBook's .import_from_csv() method is working as expected
+    context "importing from entries2.csv" do
+      it "imports the correct number of entries" do
+        book.import_from_csv("entries2.csv")
+
+        expect(book.entries.size).to eq 3
+     end
+
+       it "imports the 1st entry" do
+       book.import_from_csv("entries.csv")
+       # Check the first entry
+       entry_one = book.entries[0]
+       check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+     end
+
+     it "imports the 2nd entry" do
+       book.import_from_csv("entries.csv")
+       # Check the second entry
+       entry_two = book.entries[1]
+       check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+     end
+ 
+     it "imports the 3rd entry" do
+       book.import_from_csv("entries.csv")
+       # Check the third entry
+       entry_three = book.entries[2]
+       check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+     end
+     
+   end
 end
 
 
